f8de9bfbf0e70a7fb73c85b777daa09881ecec5b
shon_Marie <58399584+BlkCoder80@users.noreply.github.com>
2026-03-16 19:46:06 -0400

    Create band_name_generator.py

---
+++ band_name_generator.py
@@ -0,0 +1,7 @@
+print("Welcome to the Band Name Generator.")
+
+color = input("Which is your favorite color?\n")
+
+animal = input("Which is your favorite animal?\n")
+
+print("Your band name could be: " + color + " " + animal)
