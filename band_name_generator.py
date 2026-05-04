47f8895929cbb143a463f6a712763e44cdc4f9c9
shon_Marie <58399584+lashonncodes@users.noreply.github.com>
2026-05-04 19:47:50 -0400

    Delete band_name_generator.py

--- band_name_generator.py
+++
@@ -1,7 +0,0 @@
-print("Welcome to the Band Name Generator.")
-
-color = input("Which is your favorite color?\n")
-
-animal = input("Which is your favorite animal?\n")
-
-print("Your band name could be: " + color + " " + animal)
